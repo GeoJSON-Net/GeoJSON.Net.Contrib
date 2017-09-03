0d3528652519acfcdea4958d4ed43894416468b2
alvaromontero <alvaromontero@gmail.com>
2017-09-03 17:15:03 +0200

    Separate EntitiyFramework conversions into partial classes

---
+++ src/GeoJSON.Net.Contrib.EntityFramework/EntityFrameworkConvertToDbGeometry.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity.Spatial;
+using GeoJSON.Net.Contrib.EntityFramework.WkbConversions;
+using GeoJSON.Net.Geometry;
+
+namespace GeoJSON.Net.Contrib.EntityFramework
+{
+    public static partial class EntityFrameworkConvert
+    {
+        public static DbGeometry ToDbGeometry(this IGeometryObject geometryObject)
+        {
+            return DbGeometry.FromBinary(WkbEncode.Encode(geometryObject));
+        }
+    }
+}
---
+++ src/GeoJSON.Net.Contrib.EntityFramework/EntityFrameworkConvertToGeoJsonGeometry.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity.Spatial;
+using GeoJSON.Net.Contrib.EntityFramework.WkbConversions;
+using GeoJSON.Net.Geometry;
+
+namespace GeoJSON.Net.Contrib.EntityFramework
+{
+    public static partial class EntityFrameworkConvert
+    {
+        public static IGeometryObject ToGeoJSONGeometry(this DbGeography dbGeography)
+        {
+            return WkbDecode.Decode(dbGeography.AsBinary());
+        }
+
+        public static IGeometryObject ToGeoJSONGeometry(this DbGeometry dbGeometry)
+        {
+            return WkbDecode.Decode(dbGeometry.AsBinary());
+        }
+    }
+}
