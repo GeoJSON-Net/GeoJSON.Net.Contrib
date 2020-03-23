de6ffbc95816b636dcb4d81ec310a04e0d940893
Xavier Fischer <xfischer@users.noreply.github.com>
2020-03-23 22:58:21 +0100

    Update EntityFrameworkConvertToDbGeography.cs
    Kept old signature to avoid breaking change

--- src/GeoJSON.Net.Contrib.EntityFramework/EntityFrameworkConvertToDbGeography.cs
+++ src/GeoJSON.Net.Contrib.EntityFramework/EntityFrameworkConvertToDbGeography.cs
@@ -6,6 +6,12 @@ namespace GeoJSON.Net.Contrib.EntityFramework
 {
     public static partial class EntityFrameworkConvert
     {
+        [Obsolete("This method will be removed in future releases, consider migrating now to the newest signature.", false)]
+        public static DbGeography ToDbGeography(this IGeometryObject geometryObject)
+        {
+            return geometryObject.ToDbGeography(4326);
+        }
+        
         public static DbGeography ToDbGeography(this IGeometryObject geometryObject, int coordinateSystemId = 4326)
         {
             return DbGeography.FromBinary(WkbEncode.Encode(geometryObject), coordinateSystemId);
